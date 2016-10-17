5d6ff5329bc2b19b44658c9e58fab3ed6f29d0bb
Kevin-ds <kevin.ds.29@gmail.com>
2016-10-17 17:57:18 -0200

    Remove decorator

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,9 +5,7 @@ import { connect } from 'react-redux';
 import '../../css/App.css';
 import AppNavBar from './AppNavBar';
 
-@connect(store => ({
-  user: store.user
-}))
+
 class App extends Component {
   render() {
     console.log(this.props.user);
